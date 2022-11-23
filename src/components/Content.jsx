b3e140e5bd892a773b5b1a11368d38f7569796db
fadlipurnama <fadlypurnama26@gmail.com>
2022-11-23 19:13:57 +0700

    memperbaiki responsive pada file _home.css

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,15 +5,15 @@ import { BsBookFill } from 'react-icons/bs';
 import gallery1 from '../assets/Image 1.jpg'
 import gallery2 from '../assets/Image 2.jpg'
 import gallery3 from '../assets/Image 3.jpg'
-// import { Container } from 'react-bootstrap';
-// import Article from './Article';
 
 
 const Content = () => {
   return (
     <>
       <div className='box-item'>
-        <p className='item-motto__text'>Percayakan setiap fase kehamilan dan perkembangan si kecil kepada smart parents</p>
+        <div className='item-motto'>
+          <p >Percayakan setiap fase kehamilan dan perkembangan si kecil kepada smart parents</p>
+        </div>
       </div>
       <div className='box-content'>
         <div className='content-info'>
@@ -31,19 +31,19 @@ const Content = () => {
         </div>
       </div>
       <div className='box-item'>
-            <span className='item-fitur__icon'>
+            <span className='item-fitur'>
                 <FaCalculator />
                 <p className='item-fitur__title'>
                     Kalkulator Kehamilan
                 </p>
             </span>
-            <span className='item-fitur__icon'>
+            <span className='item-fitur'>
                 <RiDiscussFill />
                 <p className='item-fitur__title'>
                     Forum Diskusi
                 </p>
             </span>
-            <span className='item-fitur__icon'>
+            <span className='item-fitur'>
                 <BsBookFill />
                 <p className='item-fitur__title'>
                     Artikel Keluarga
