08ec62cabf94a2e6ff1602fb80c36205688cacc3
fadlipurnama <fadlypurnama26@gmail.com>
2022-11-26 22:30:09 +0700

    Update ArticlePage.jsx

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,23 +1,17 @@
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 
-const API =
-  "https://data-base-article-default-rtdb.firebaseio.com/data-article.json";
 const Article = (props) => {
-  axios.get(API).then((res) => {
-    console.log(res);
-  });
-
   return (
     <div className="card-article">
       <div className="img-thumb">
-        <img src={props.images} alt="" />
+        <img src={props.images} alt={props.alt} />
       </div>
       <div className="article-text">
         <p className="article-text__title">{props.title}</p>
         <p className="article-text__desc">{props.desc}</p>
       </div>
-      <button>Learn More</button>
+      <button><a href={props.url} >Learn More</a></button>
     </div>
   );
 };
--- src/pages/ArtikelPage.jsx
+++ src/pages/ArtikelPage.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import Article from '../components/Article';
+import React, { useEffect, useState } from "react";
+import Article from "../components/Article";
+import axios from "axios";
 
-const ArtikelPage = () => {
+function ArtikelPage() {
+  const [post, setPost] = useState([]);
+  const API =
+    "https://data-base-article-default-rtdb.firebaseio.com/data-article/-NHnu7_0z809hEDcshST.json";
+
+  useEffect(() => {
+    axios.get(API).then((result) => {
+      setPost(result.data);
+    });
+  }, [setPost]);
+  console.log(post);
+
   return (
-    <div className='box-content'>
+    <div className="box-content">
       <h1>ArtikelPage</h1>
-      <Article images={''} title='TESTING TITLE' desc='TESTING DESCRIPTION' />
+      {post.map((post) => {
+        return (
+          <Article
+            images={''}
+            title={post.title}
+            desc={post.description}
+            url={post.url}
+          />
+        );
+      })}
     </div>
-  )
+  );
 }
 
 export default ArtikelPage;
