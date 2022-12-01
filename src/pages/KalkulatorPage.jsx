6f8ec906e0ca3e53c0ff32fea732d69ec28d3704
SitiSuharyanti <siti.08@students.amikom.ac.id>
2022-12-01 17:02:22 +0700

    Mengedit halaman kalkulator

--- src/pages/KalkulatorPage.jsx
+++ src/pages/KalkulatorPage.jsx
@@ -4,25 +4,30 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import KalkulatorContent from '../components/KalkulatorContent';
 import KalkulatorResult from '../components/KalkulatorResult';
-import Vector2 from '../assets/Vector 2.png';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-// import KalkulatorForm from '../components/KalkulatorForm';
+import '../style/components/_calendar.css';
 
 const KalkulatorPage = () => {
   const [date, newDate] = useState(new Date());
+  const [showResult, setShowResult] = useState(false);
+
+  const onClick = () => setShowResult(true);
+
   return (
     <>
       <Container>
         <Row className='kalkulator-page'>
           <Col>
-            <p>Pilih Tanggal Terakhir Menstruasi</p>
-            <Calendar onChange={newDate} value={date} className='calendar' />
-            <KalkulatorResult date={date} />
+            <strong>
+              <p>Pilih Tanggal Terakhir Menstruasi</p>
+            </strong>
+            <div className='my-3'>
+              <Calendar onChange={newDate} value={date} onClickDay={onClick} maxDate={new Date()} />
+              {showResult ? <KalkulatorResult date={date} /> : null}
+            </div>
           </Col>
           <Col>
             <KalkulatorContent />
-            <img src={Vector2} alt='Vector Mom' className='kalkulator-page__vector-2' />
           </Col>
         </Row>
       </Container>
