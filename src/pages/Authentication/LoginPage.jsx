fed2cff05e57fcd6523d9356f26fa3d479212f85
fadlipurnama <fadlypurnama26@gmail.com>
2022-12-01 23:03:20 +0700

    memperbaiki fitur login

--- src/pages/Authentication/LoginPage.jsx
+++ src/pages/Authentication/LoginPage.jsx
@@ -1,36 +1,40 @@
 import React from "react";
+import loginImageVektor from "../../assets/login-image-vektor.png";
+import logoImage from "../../assets/logo.png";
 
 const LoginPage = () => {
   return (
-    <main>
-      <div className='container-login'>
-        <div>
-            <div>
-                <h1>Kembali ke Home</h1>
-            </div>
-            <div className="card-login">
-                <img src="" alt="" />
-                <h1>Masuk</h1>
-                <form>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input type="text" />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" />
-                    </div>
-                    <button>Masuk</button>
-                    <button>Masuk dengan GOOGLE</button>
-                </form>
-                <p>Belum punya akun? Daftar</p>
-            </div>
+    <div className="container-login">
+      <div className="login-form">
+        <div className="back-to-login">
+          <h1>Kembali ke Home</h1>
         </div>
-        <div>
-            <img src="" alt="" />
+        <div className="form-card">
+          <div className="item-logo">
+            <img src={logoImage} alt="" />
+          </div>
+          <form>
+            <h1 className="login-title">Masuk</h1>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input type="password" />
+              <p>forget password</p>
+            </div>
+            <button>Masuk</button>
+            <button>Masuk dengan GOOGLE</button>
+          </form>
+          <p>Belum punya akun? Daftar</p>
         </div>
       </div>
-    </main>
+
+      <div className="login-image">
+        <img src={loginImageVektor} alt="" />
+      </div>
+    </div>
   );
 };
 
