e3824f3d5384fac0191553e5c63969421c74cec3
fadlipurnama <fadlypurnama26@gmail.com>
2022-12-02 20:54:17 +0700

    Update file authentication login page dan register page

--- src/pages/Authentication/LoginPage.jsx
+++ src/pages/Authentication/LoginPage.jsx
@@ -1,38 +1,60 @@
 import React from "react";
-import loginImageVektor from "../../assets/login-image-vektor.png";
+import heroAuth from "../../assets/login-image-vektor.png";
 import logoImage from "../../assets/logo.png";
+import { AiOutlineHome } from "@react-icons/all-files/ai/AiOutlineHome";
+import { FcGoogle } from "@react-icons/all-files/fc/FcGoogle";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
     <div className="container-login">
-      <div className="login-form">
-        <div className="back-to-login">
-          <h1>Kembali ke Home</h1>
-        </div>
-        <div className="form-card">
-          <div className="item-logo">
-            <img src={logoImage} alt="" />
-          </div>
-          <form>
-            <h1 className="login-title">Masuk</h1>
-            <div>
-              <label htmlFor="email">Email</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <input type="password" />
-              <p>forget password</p>
-            </div>
-            <button>Masuk</button>
-            <button>Masuk dengan GOOGLE</button>
-          </form>
-          <p>Belum punya akun? Daftar</p>
-        </div>
+      <div className="back-to-home">
+        <Link to="/">
+          <AiOutlineHome className="icon-home" />
+          Kembali ke Home
+        </Link>
       </div>
 
-      <div className="login-image">
-        <img src={loginImageVektor} alt="" />
+      <div className="form-login">
+        <div className="item-logo">
+          <img src={logoImage} alt="" />
+        </div>
+        <h1 className="login-title">Masuk</h1>
+        <form>
+          <div className="form-group">
+            <label className="mb-2" htmlFor="email">Email</label>
+            <input type="text" placeholder="Masukan Email" />
+          </div>
+          <div className="form-group">
+            <label className="mb-2" htmlFor="password">Password</label>
+            <input type="password" placeholder="Masukan Password" />
+          </div>
+          <button className="btn-login-email">Masuk</button>
+          <div className="d-flex px-0">
+            <div className="col-5 px-0">
+              <hr />
+            </div>
+            <div className="col-2 text-center">
+              <div className="mt-1">atau</div>
+            </div>
+            <div className="col-5 px-0">
+              <hr />
+            </div>
+          </div>
+          <div className="d-flex align-items-center justify-content-center gap-4 login-google shadow mb-3">
+            <FcGoogle className="icon-google" />
+            <p>Masuk dengan Google</p>
+          </div>
+        </form>
+
+        <p className="to-register">
+          Belum punya akun? Silahkan{"   "}
+          <Link to="/register">Daftar</Link>
+        </p>
+      </div>
+
+      <div className="hero-login">
+        <img src={heroAuth} alt="hero-login" />
       </div>
     </div>
   );
--- src/pages/Authentication/RegisterPage.jsx
+++ src/pages/Authentication/RegisterPage.jsx
@@ -1,9 +1,61 @@
 import React from "react";
+import heroAuth from "../../assets/login-image-vektor.png";
+import logoImage from "../../assets/logo.png";
+import { AiOutlineHome } from "@react-icons/all-files/ai/AiOutlineHome";
+import { FcGoogle } from "@react-icons/all-files/fc/FcGoogle";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
-    <div>
-      <h1>RegisterPage</h1>
+    <div className="container-register">
+      <div className="register-back-to-home">
+        <Link to="/">
+          <AiOutlineHome className="icon-home" />
+          Kembali ke Home
+        </Link>
+      </div>
+
+      <div className="hero-register">
+        <img src={heroAuth} alt="hero-register" />
+      </div>
+
+      <div className="form-register">
+        <div className="item-logo">
+          <img src={logoImage} alt="" />
+        </div>
+        <h1 className="register-title">Daftar</h1>
+        <form>
+          <div className="form-group">
+            <label className="mb-2" htmlFor="email">Email</label>
+            <input type="text" placeholder="Masukan Email" />
+          </div>
+          <div className="form-group">
+            <label className="mb-2" htmlFor="password">Password</label>
+            <input type="password" placeholder="Masukan Password" />
+          </div>
+          <button className="btn-register-email">Daftar</button>
+          <div className="d-flex px-0">
+            <div className="col-5 px-0">
+              <hr />
+            </div>
+            <div className="col-2 text-center">
+              <div className="mt-1">atau</div>
+            </div>
+            <div className="col-5 px-0">
+              <hr />
+            </div>
+          </div>
+          <div className="login-google d-flex align-items-center justify-content-center gap-4 shadow mb-3">
+            <FcGoogle className="icon-google" />
+            <p>Masuk dengan Google</p>
+          </div>
+        </form>
+
+        <p className="to-register">
+          Sudah punya akun? Silahkan{"   "}
+          <Link to="/login">Login</Link>
+        </p>
+      </div>
     </div>
   );
 };
